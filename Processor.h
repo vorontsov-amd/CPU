039a68273dae1adee3a9517f158f1b4c396c1720
vorontsov-amd <89128818+vorontsov-amd@users.noreply.github.com>
2021-12-25 03:18:52 +0300

    Update Processor.h

--- Processor.h
+++ Processor.h
@@ -1,5 +1,26 @@
 #pragma once
-#include "C:\Users\voron\Documents\Code\C\assembler\assembler\CPU.h"
+#include "..\..\CPU.h"
+#include "..\..\enum.h"
 
 
+struct CPU
+{
+	Stack stack;
+	double registr[4];
+	int ip;
+	int* code;
+	size_t code_capacity;
+};
+
+
+#define CODE processor.code
+#define IP processor.ip
+#define REGISTER processor.registr
+#define POP(ptr) StackPop(&processor.stack, (element*)(&##ptr));
+#define PUSH(num) StackPush(&processor.stack, (element)(num));
+#define MODE AnalysMode(processor.code[processor.ip])
+
+void InitCPU(CPU& processor, size_t start_capacity);
+void ClearCPU(CPU& processor);
 void Execute(CPU& processor);
+int AnalysMode(int cmd);
