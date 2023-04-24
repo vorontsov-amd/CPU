756157b3a305c79a734c2495d5aed11d559f9d8f
Amadey Vorontsov <vorontsov.aa@phystech.edu>
2023-04-24 19:51:13 +0300

    add more info to readme

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-### This version of a processor has the next commands: ###
+# CPU
+
+This repository includes my CPU emulator, Assembler, and Disassembler. The processor has a standard set of commands such as push, pop, math commands (add, mul, ...), jump commands (call, jae), draw, out, and more. It also has RAM and video memory, so you can fill it and then display it, thus displaying pictures or even videos. The assembler translates a text file into bytecode, which the processor executes. The disassembler performs the reverse task.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Commands](#commands)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The CPU emulator in this repository simulates the behavior of a processor that can execute a standard set of commands, has RAM and video memory, and can perform various tasks such as displaying pictures or videos. The Assembler and Disassembler tools are used to convert text files into bytecode and vice versa.
+
+## Requirements
+
+To use the CPU emulator, Assembler, and Disassembler in this repository, you need to have the following:
+
+- C++ Compiler (GCC, Clang, or MSVC)
+- SFML Library
+
+## Installation
+
+To install the CPU emulator, Assembler, and Disassembler, you can clone this repository using the following command:
+
+```
+git clone https://github.com/vorontsov-amd/CPU.git
+```
+
+Then, navigate to the `CPU` directory and compile the code using your preferred C++ compiler. You also need to install the SFML library and link it to your project.
+
+## Usage
+
+The CPU emulator, Assembler, and Disassembler in this repository can be used to write and execute assembly language programs. Here's an overview of the tools available in this repository:
+
+- `Processor.cpp`: This file contains the implementation of the CPU emulator.
+- `assembler.cpp`: This file contains the implementation of the Assembler tool.
+- `disassembler.cpp`: This file contains the implementation of the Disassembler tool.
+
+To use these tools, you need to write an assembly language program in a text file, assemble it using the Assembler tool, and then execute it using the CPU emulator. You can also disassemble the bytecode back into assembly language using the Disassembler tool.
+
+## Commands
 - push
   - push value        - push the number to stack
   - push [index]      - push the value from ram[index]
@@ -33,3 +78,17 @@ Take two numbers from stack: a, then b
 - return - return from function to the main function
 
 - HLT - ends the program
+
+- scan
+- draw
+- stop
+
+
+## Contributing
+
+If you want to contribute to this project, feel free to submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/vorontsov-amd/CPU/blob/main/LICENSE).
+
---
+++ disassembler.cpp
@@ -0,0 +1,150 @@
+﻿#include "disassm.h"
+
+int main()
+{
+	FILE* machine_code = nullptr;
+	OpenFile(&machine_code, "machine_code.bin", "rb");
+
+	FILE* text_code = nullptr;
+	OpenFile(&machine_code, "text_code.txt", "w");
+
+	int BufferSize = 0;
+	fread(&BufferSize, 1, sizeof(int), machine_code);
+	rewind(machine_code);
+
+	CPU disassembler = {};
+	InitCPU(disassembler, BufferSize);
+	fread(disassembler.code, disassembler.code_capacity, sizeof(int), machine_code);
+
+	Translate(disassembler, text_code);
+
+	fclose(machine_code);
+	fclose(text_code);
+}
+
+
+
+void Translate(CPU disassembler, FILE* output)
+{
+	disassembler.ip = 1;
+	while (disassembler.ip < disassembler.code_capacity)
+	{
+		switch (disassembler.code[disassembler.ip])
+		{
+			case CMD_add:
+			{
+				fputs("Add\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_div:
+			{
+				fputs("Div\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_hlt:
+			{
+				fputs("Hlt\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_in:
+			{
+				fputs("In\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_mul:
+			{
+				fputs("Mul\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_out:
+			{
+				fputs("Out\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_pop:
+			{
+				fputs("Pop\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_sqrt:
+			{
+				fputs("Sqrt\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_sub:
+			{
+				fputs("Sub\n", output);
+				disassembler.ip++;
+				break;
+			}
+			case CMD_push:
+			{
+				fputs("Push ", output);
+				disassembler.ip++;
+				fprintf(output, "%d", disassembler.code[disassembler.ip++]);
+				break;
+			}
+			case CMD_rpush:
+			{
+				fputs("Push ", output);
+				disassembler.ip++;
+				switch (disassembler.code[disassembler.ip++])
+				{
+				case rax_index:
+					fputs("rax\n", output);
+					break;
+				case rbx_index:
+					fputs("rbx\n", output);
+					break;
+				case rcx_index:
+					fputs("rcx\n", output);
+					break;
+				case rdx_index:
+					fputs("rdx\n", output);
+					break;
+				default:
+					fprintf(output, "UNCNOWN CMD %d", disassembler.code[disassembler.ip - 1]);
+					break;
+				}
+				break;
+			}
+			case CMD_rpop:
+			{
+				fputs("Pop ", output);
+				disassembler.ip++;
+				switch (disassembler.code[disassembler.ip++])
+				{
+				case rax_index:
+					fputs("rax\n", output);
+					break;
+				case rbx_index:
+					fputs("rbx\n", output);
+					break;
+				case rcx_index:
+					fputs("rcx\n", output);
+					break;
+				case rdx_index:
+					fputs("rdx\n", output);
+					break;
+				default:
+					fprintf(output, "UNCNOWN CMD %d", disassembler.code[disassembler.ip - 1]);
+					break;
+				}
+				break;
+			}
+			default:
+			{
+				fputs("Programm cannot translate incorrectly compiled code\n", output);
+				break;
+			}
+		}
+	}
+}
